62123780e6444fb162164f6f9ef27393133f374c
Paul <pauldecalf@outlook.fr>
2025-01-29 20:04:41 +0100

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activer CORS avant d'écouter les requêtes
+  app.enableCors({
+    origin: 'http://localhost:3001', // Autoriser uniquement le frontend
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
